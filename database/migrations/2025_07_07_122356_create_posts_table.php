a8e44bce37438f6948b588eb3a30eb8e08f2bcde
shayanahmad1999 <shayanahmad235@gmail.com>
2025-07-07 19:27:49 +0500

    add user id as foreign key in posts

--- database/migrations/2025_07_07_122356_create_posts_table.php
+++ database/migrations/2025_07_07_122356_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,10 +14,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('content');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
